ec30ff3ef8d3f43c2c1c11d0eb1784d0b14ef249
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-03-04 15:59:26 -0500

    Add test plugin

---
+++ test_plugin/src/dll_main.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NOCOMM
+#define NOCLIPBOARD
+#define NODRAWTEXT
+#define NOMB
+
+#include <Windows.h>
+
+void __stdcall plugin_main(HMODULE dll_handle) {
+    printf("Test plugin: injection complete.\n");
+
+	const uint8_t* client = (uint8_t*)GetModuleHandle("PDUWP.exe");
+	const uint8_t* gsdata = (client + 0x4C5240);
+
+	while (!GetAsyncKeyState(VK_END)) {
+		Sleep(1000);
+		if (gsdata[0x10] == 0) {
+            printf("plugin: Set version number to 1.40.\n");
+			*(uint32_t*)&gsdata[0x10] = 140;
+		}
+		else {
+            printf("plugin: Set version number to 0.00.\n");
+			*(uint32_t*)&gsdata[0x10] = 0;
+		}
+	}
+
+	// Uninject
+	FreeLibraryAndExitThread((HMODULE)(dll_handle), EXIT_SUCCESS);
+}
+
+int32_t __stdcall DllMain(HINSTANCE dll_handle, uint32_t reason, void* reserved)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		// Disable DLL notifications for new threads starting up, because we have no need to run special code here.
+		DisableThreadLibraryCalls(dll_handle);
+
+		// Start injected code
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) plugin_main, dll_handle, 0, NULL);
+	}
+	return TRUE;
+}
