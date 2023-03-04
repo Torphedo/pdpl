066a4cfcb9ac5a950b78ddf791b2d97182bebf94
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-03-04 01:31:27 -0500

    Injector: inject on boot

--- inject/src/main.c
+++ inject/src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 {
     if (argc == 1)
     {
-        printf("Usage:\n\n\tinject [dll file] [executable target]\n\nExample:\n\n\tinject pdpm.dll PDUWP.exe\n");
+        printf("Usage:\n\n\tinject [dll file]\n\nExample:\n\n\tinject pdpm.dll\n");
         system("pause");
         return EXIT_FAILURE;
     }
@@ -24,14 +24,32 @@ int main(int argc, char** argv)
     char full_path[MAX_PATH] = { 0 };
     GetFullPathNameA(argv[1], MAX_PATH, full_path, NULL);
 
-	printf("Injecting mods into Phantom Dust...\n");
-	if(!dll_inject_remote(get_pid_by_name(argv[2]), full_path, MAX_PATH + 1))
-	{
-		printf("Failed.\n");
+    // Kill Phantom Dust if it's already running
+    uint32_t process_id = get_pid_by_name("PDUWP.exe");
+    if (process_id != 0) {
+        // Get a handle with permission to terminate the game
+        void* process = OpenProcess(PROCESS_TERMINATE, false, process_id);
+        if (process == NULL) {
+            printf("main(): Unable to open process ID %d for termination.\n", process_id);
+            return false;
+        }
+        // Terminate Phantom Dust with an exit code of 0
+        TerminateProcess(process, 0);
+    }
+
+    // Run Phantom Dust
+    system("explorer shell:AppsFolder\\Microsoft.MSEsper_8wekyb3d8bbwe!App");
+    while (get_pid_by_name("PDUWP.exe") == 0) {
+        Sleep(1);
+    }
+    process_id = get_pid_by_name("PDUWP.exe");
+
+	if(!dll_inject_remote(process_id, full_path, MAX_PATH + 1)) {
+		printf("Injecting mods into Phantom Dust... Failed.\n");
 		system("pause");
 		return EXIT_FAILURE;
 	}
-	printf("Success!\n");
+	printf("Injecting mods into Phantom Dust... Success!\n");
 
 	return EXIT_SUCCESS;
 }
