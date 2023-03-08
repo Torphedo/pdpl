a0725f89d964e4938d3cfcdea9ecdf4f2fd0da17
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-03-08 00:39:19 -0500

    VFS: Mount everything at root, fix priority
    - Fixes an issue causing mod files to not be detected
    - Mount everything at the filesystem root to allow replacement of compiled shader objects (.cso DirectX bytecode)

--- pdpm/src/filesystem.c
+++ pdpm/src/filesystem.c
@@ -28,34 +28,24 @@ void vfs_setup() {
     // Get game RoamingState path
     char app_path[MAX_PATH] = { 0 };
 
-    // Get path to Phantom Dust files by getting location of PDUWP.exe and truncating the filename
-    GetModuleFileNameA(NULL, app_path, MAX_PATH);
-    path_truncate(app_path, MAX_PATH);
-    app_path[strlen(app_path) - 1] = 0; // Cut off trailing backslash
-
-    path_fix_backslashes(app_path);
-    // Mount to root of VFS, and prepend to search path list so that it's the last resort.
-    PHYSFS_mount(app_path, "/", false);
-    printf("%s: Mounting vanilla game (%s) at the filesystem root.\n", vfs_msg, app_path);
-
-    memset(app_path, 0, MAX_PATH); // Clear string so there are no leftovers of the old string
     get_ms_esper_path(app_path);
 
     // Enabling writing to this directory and make the mod / plugin folders if necessary
     PHYSFS_setWriteDir(app_path);
     PHYSFS_mkdir("mods/plugins");
+    PHYSFS_mkdir("fake");
 
     // Add mod folder to the search path
     strcat(app_path, "mods");
-    if (PHYSFS_mount(app_path, "/Assets/Data/", true) == 0) {
+    if (PHYSFS_mount(app_path, "/", true) == 0) {
         printf("%s: Failed to add %s to the virtual filesystem. (%s)\n", vfs_err, app_path, PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
     }
     else {
-        printf("%s: Mounted %s at /Assets/Data/\n", vfs_msg, app_path);
+        printf("%s: Mounted %s at root.\n", vfs_msg, app_path);
     }
 
     // Recursive Archive Mounter
-    static const char* dir = "/Assets/Data";
+    static const char* dir = "/";
     char** file_list = PHYSFS_enumerateFiles(dir);
 
     for (char** i = file_list; *i != NULL; i++) {
@@ -63,17 +53,28 @@ void vfs_setup() {
             char full_path[MAX_PATH] = {0};
 
             // Get full virtual filesystem path.
-            sprintf(full_path, "%s/%s", dir, *i);
-            printf("%s: Mounting %s at /Assets/Data\n", vfs_msg, full_path);
+            sprintf(full_path, "%s%s", dir, *i);
+            printf("%s: Mounting %s at root.\n", vfs_msg, full_path);
 
             // Real search path + / or \ + filename
             sprintf(full_path, "%s%s%s",PHYSFS_getRealDir(full_path), PHYSFS_getDirSeparator(), *i);
 
             // Mount to the current virtual directory.
-            PHYSFS_mount(full_path, dir, true);
+            PHYSFS_mount(full_path, "/", true);
         }
     }
     PHYSFS_freeList(file_list);
+
+    memset(app_path, 0, MAX_PATH); // Clear string so there are no leftovers of the old string
+    // Get path to Phantom Dust files by getting location of PDUWP.exe and truncating the filename
+    GetModuleFileNameA(NULL, app_path, MAX_PATH);
+    path_truncate(app_path, MAX_PATH);
+    app_path[strlen(app_path) - 1] = 0; // Cut off trailing backslash
+
+    path_fix_backslashes(app_path);
+    // Mount to root of VFS, and prepend to search path list so that it's the last resort.
+    PHYSFS_mount(app_path, "/", true);
+    // printf("%s: Mounting vanilla game (%s).\n", vfs_msg, app_path);
 
     printf("%s: Finished setting up.\n", vfs_msg);
     printf("%s: Unlocking files for read/write...\n\n", vfs_msg);
