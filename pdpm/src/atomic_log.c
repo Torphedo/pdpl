31a778fbf156e896236642233bfbd0f3849f54fe
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-03-19 17:35:08 -0400

    Remove atomic_log.c

--- pdpm/src/atomic_log.c
+++
@@ -1,9 +0,0 @@
-#include <physfs.h>
-#include <string.h>
-
-void atomic_log(const char* message, const char* file) {
-    PHYSFS_File* physfs_handle = PHYSFS_openAppend(file);
-    PHYSFS_writeBytes(physfs_handle, message, strlen(message));
-    PHYSFS_writeBytes(physfs_handle, "\n", 1);
-    PHYSFS_close(physfs_handle);
-}
--- pdpm/src/atomic_log.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Write a string out to a file, then immediately close the file handle to write it to disk.
-// The specified file will be created in the PhysicsFS write path.
-void atomic_log(char* message, char* file);
