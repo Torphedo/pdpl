60d5e398ce9c9e8414339e1830cd864631b9ee09
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-03-19 17:36:37 -0400

    Remove unused function declarations
    (These functions no longer exist)

--- pdpm/src/path.h
+++ pdpm/src/path.h
@@ -3,20 +3,11 @@
 
 void get_ms_esper_path(char* string);
 
-// Checks if a path ends in "mods\plugins\", "mods\plugins", "mods/plugins/", or "mods/plugins"
-bool path_is_plugin_folder(const char* path);
-
 bool path_has_extension(const char* path, const char* extension);
 
 /// Replaces all backslashes in a string with forward slashes.
 /// \param path String to edit. Must be null-terminated.
 void path_fix_backslashes(char* path);
-
-/// Returns the position of the next folder from a specified starting point in the string.
-/// \param path Path string to check
-/// \param start_pos Position to start seeking
-/// \return Position of the next slash or backslash in the string from the specified start position.
-uint16_t path_pos_next_folder(const char* path, uint16_t start_pos);
 
 /// Truncates a filename or folder name from a path, leaving a trailing "\\" or "/".
 /// \param path The path to truncate. This string will be edited. If the string does not contain any slashes or backslashes, it will be completely filled with null characters.
