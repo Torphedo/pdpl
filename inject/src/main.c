484cee3eab87c703bf59c2ae4eeead4001863173
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-07-19 14:51:46 -0400

    Copy loader DLL to mod folder on start

--- inject/src/main.c
+++ inject/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Reduce the size of Windows.h to improve compile time
 #define WIN32_LEAN_AND_MEAN
@@ -8,11 +9,36 @@
 #define NODRAWTEXT
 #define NOMB
 #include <Windows.h>
+#include <shlobj.h> // SHGetFolderPathA()
+#include <sys/stat.h>
+#include <direct.h> // _mkdir()
 
 #include "remote_injection.h"
 
+bool file_exists(const char* path) {
+    struct stat buffer = {0};
+    return (stat(path, &buffer) == EXIT_SUCCESS);
+}
+
 int main(int argc, char** argv) {
     static int result = EXIT_SUCCESS;
+
+    // Copy pd_loader_core.dll to a place where it can be read by the game, if it doesn't exist there.
+    static char mods_path[MAX_PATH] = {0};
+    SHGetFolderPathA(0, CSIDL_LOCAL_APPDATA, NULL, 0, mods_path);
+    strcat(mods_path, "\\Packages\\Microsoft.MSEsper_8wekyb3d8bbwe\\RoamingState\\mods");
+    _mkdir(mods_path);
+    strcat(mods_path, "\\pd_loader_core.dll");
+    if (!file_exists(mods_path)) {
+        if (!file_exists("pd_loader_core.dll")) {
+            printf("Couldn't find pd_loader_core.dll. Please place this file in the mods folder or next to pdpl.exe.\n");
+            system("pause");
+        }
+        else {
+            printf("Copying pd_loader_core.dll into the mods folder for first-time setup.\n");
+            CopyFile("pd_loader_core.dll", mods_path, FALSE);
+        }
+    }
 
     // Kill Phantom Dust if it's already running
     uint32_t process_id = get_pid_by_name("PDUWP.exe");
