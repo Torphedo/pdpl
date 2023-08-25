3a7c3868838a422a54b6421ba5452d0766e82a79
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-08-25 15:51:20 -0400

    Refactor to snake case

--- src/self_inject.c
+++ src/self_inject.c
@@ -34,48 +34,48 @@ uint32_t get_pid_by_name(const char* ProcessName) {
 
 bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* parameter) {
     // Open the target process - this is process we will be injecting this PE into
-    HANDLE targetProcess = OpenProcess(PROCESS_VM_WRITE | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, FALSE, process_id);
+    HANDLE target_process = OpenProcess(PROCESS_VM_WRITE | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, FALSE, process_id);
 
     // Get current image's base address
-    void* imageBase = GetModuleHandle(NULL);
-    IMAGE_DOS_HEADER* dosHeader = imageBase;
-    IMAGE_NT_HEADERS* ntHeader = (imageBase + dosHeader->e_lfanew);
+    void* image_base = GetModuleHandle(NULL);
+    IMAGE_DOS_HEADER* dos_header = image_base;
+    IMAGE_NT_HEADERS* nt_header = (image_base + dos_header->e_lfanew);
 
     // Allocate a new memory block and copy the current PE image to this new memory block
-    void* localImage = VirtualAlloc(NULL, ntHeader->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_READWRITE);
-    memcpy(localImage, imageBase, ntHeader->OptionalHeader.SizeOfImage);
+    void* local_image = VirtualAlloc(NULL, nt_header->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_READWRITE);
+    memcpy(local_image, image_base, nt_header->OptionalHeader.SizeOfImage);
 
     // Allocate a new memory block in the target process. This is where we will be injecting this PE
-    void* targetImage = VirtualAllocEx(targetProcess, NULL, ntHeader->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    void* target_image = VirtualAllocEx(target_process, NULL, nt_header->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
     // Calculate delta between addresses of where the image will be located in the target process and where it's located currently
-    uintptr_t deltaImageBase = (uintptr_t)targetImage - (uintptr_t)imageBase;
+    uintptr_t delta_image_base = (uintptr_t)target_image - (uintptr_t)image_base;
 
-    // Relocate localImage, to ensure that it will have correct addresses once it's in the target process
-    IMAGE_BASE_RELOCATION* relocationTable = (IMAGE_BASE_RELOCATION*)(localImage + ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
-    DWORD relocationEntriesCount = 0;
+    // Relocate local_image, to ensure that it will have correct addresses once it's in the target process
+    IMAGE_BASE_RELOCATION* relocation_table = (IMAGE_BASE_RELOCATION*)(local_image + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+    DWORD relocation_entries_count = 0;
 
-    while (relocationTable->SizeOfBlock > 0)
+    while (relocation_table->SizeOfBlock > 0)
     {
-        relocationEntriesCount = (relocationTable->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(USHORT);
-        BASE_RELOCATION_ENTRY* relocationRVA = (BASE_RELOCATION_ENTRY*)(relocationTable + 1);
+        relocation_entries_count = (relocation_table->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(USHORT);
+        BASE_RELOCATION_ENTRY* relative_addresses = (BASE_RELOCATION_ENTRY*)(relocation_table + 1);
 
-        for (short i = 0; i < relocationEntriesCount; i++)
+        for (short i = 0; i < relocation_entries_count; i++)
         {
-            if (relocationRVA[i].Offset)
+            if (relative_addresses[i].Offset)
             {
-                uintptr_t* patchedAddress = (uintptr_t*)(localImage + relocationTable->VirtualAddress + relocationRVA[i].Offset);
-                *patchedAddress += deltaImageBase;
+                uintptr_t* patched_address = (uintptr_t*)(local_image + relocation_table->VirtualAddress + relative_addresses[i].Offset);
+                *patched_address += delta_image_base;
             }
         }
-        relocationTable = (IMAGE_BASE_RELOCATION*)((uintptr_t)relocationTable + relocationTable->SizeOfBlock);
+        relocation_table = (IMAGE_BASE_RELOCATION*)((uintptr_t)relocation_table + relocation_table->SizeOfBlock);
     }
 
-    // Write the relocated localImage into the target process
-    WriteProcessMemory(targetProcess, targetImage, localImage, ntHeader->OptionalHeader.SizeOfImage, NULL);
+    // Write the relocated local_image into the target process
+    WriteProcessMemory(target_process, target_image, local_image, nt_header->OptionalHeader.SizeOfImage, NULL);
 
     // Start the injected PE inside the target process
-    CreateRemoteThread(targetProcess, NULL, 0, (LPTHREAD_START_ROUTINE)((DWORD_PTR)entry_point + deltaImageBase), parameter, 0, NULL);
+    CreateRemoteThread(target_process, NULL, 0, (LPTHREAD_START_ROUTINE)((DWORD_PTR)entry_point + delta_image_base), parameter, 0, NULL);
 
     return true;
 }
