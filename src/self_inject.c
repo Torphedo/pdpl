9d063f448638d61abac81c393e8d8b16110fbc50
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-08-25 23:15:12 -0400

    Do actual safety checks

--- src/self_inject.c
+++ src/self_inject.c
@@ -35,20 +35,31 @@ uint32_t get_pid_by_name(const char* ProcessName) {
 bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* parameter) {
     // Open the target process - this is the process we will be injecting this PE into
     HANDLE target_process = OpenProcess(PROCESS_VM_WRITE | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, FALSE, process_id);
+	if (target_process == INVALID_HANDLE_VALUE) {
+	  return false;
+	}
 
     // Get current image's base address
     void* image_base = GetModuleHandle(NULL);
     IMAGE_DOS_HEADER* dos_header = image_base;
     IMAGE_NT_HEADERS* nt_header = (image_base + dos_header->e_lfanew);
 
-    // Allocate a new memory block and copy the current PE image to this new memory block
+	// Allocate buffers for the PE data in the local and remote processes
     void* local_image = malloc(nt_header->OptionalHeader.SizeOfImage);
-    memcpy(local_image, image_base, nt_header->OptionalHeader.SizeOfImage);
-
-    // Allocate a new memory block in the target process. This is where we will be injecting this PE
     void* target_image = VirtualAllocEx(target_process, NULL, nt_header->OptionalHeader.SizeOfImage, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
-    // Calculate delta between addresses of where the image will be located in the target process and where it's located currently
+	// Early exit if we fail an allocation. Free does nothing if passed NULL.
+	if (local_image == NULL || target_image == NULL) {
+	  free(local_image);
+	  free(target_image);
+	  CloseHandle(target_process);
+	  return false;
+	}
+
+	// Copy PE data into our local buffer
+    memcpy(local_image, image_base, nt_header->OptionalHeader.SizeOfImage);
+
+    // Calculate delta between the base addresses of our two image addresses.
     uintptr_t delta_image_base = (uintptr_t)target_image - (uintptr_t)image_base;
 
     // Relocate local_image, to ensure that it will have correct addresses once it's in the target process
@@ -68,7 +79,7 @@ bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* 
         relocation_table = (IMAGE_BASE_RELOCATION*)((uintptr_t)relocation_table + relocation_table->SizeOfBlock);
     }
 
-    // Write the relocated local_image into the target process
+    // Write the relocated PE into the target process
     WriteProcessMemory(target_process, target_image, local_image, nt_header->OptionalHeader.SizeOfImage, NULL);
     free(local_image);
 
