b7bf3c7036cd96a3477eb76ce58d61d2fec82da1
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-08-29 20:31:52 -0400

    Minor refactors

--- src/self_inject.c
+++ src/self_inject.c
@@ -15,20 +15,20 @@ typedef struct BASE_RELOCATION_ENTRY {
 }BASE_RELOCATION_ENTRY;
 
 uint32_t get_pid_by_name(const char* ProcessName) {
-    PROCESSENTRY32 pt = {
+    PROCESSENTRY32 entry = {
             .dwSize = sizeof(PROCESSENTRY32)
     };
-    HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (Process32First(hsnap, &pt)) { // must call this first
-        while (Process32Next(hsnap, &pt))
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (Process32First(snapshot, &entry)) { // must call this first
+        while (Process32Next(snapshot, &entry))
         {
-            if (!lstrcmpi(pt.szExeFile, ProcessName)) {
-                CloseHandle(hsnap);
-                return pt.th32ProcessID;
+            if (!lstrcmpi(entry.szExeFile, ProcessName)) {
+                CloseHandle(snapshot);
+                return entry.th32ProcessID;
             }
         }
     }
-    CloseHandle(hsnap); // close handle on failure
+    CloseHandle(snapshot); // close handle on failure
     return 0;
 }
 
@@ -40,9 +40,9 @@ bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* 
 	}
 
     // Get current image's base address
-    uint8_t* image_base = GetModuleHandle(NULL);
-    IMAGE_DOS_HEADER* dos_header = image_base;
-    IMAGE_NT_HEADERS* nt_header = (image_base + dos_header->e_lfanew);
+    uint8_t* image_base = (uint8_t*) GetModuleHandle(NULL);
+    IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*) image_base;
+    IMAGE_NT_HEADERS* nt_header = (IMAGE_NT_HEADERS*) (image_base + dos_header->e_lfanew);
 
 	// Allocate buffers for the PE data in the local and remote processes
     uint8_t* local_image = malloc(nt_header->OptionalHeader.SizeOfImage);
@@ -60,27 +60,27 @@ bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* 
     memcpy(local_image, image_base, nt_header->OptionalHeader.SizeOfImage);
 
     // Calculate delta between the base addresses of our two image addresses.
-    uintptr_t delta_image_base = (uintptr_t)target_image - (uintptr_t)image_base;
+    uintptr_t reloc_bias = (uintptr_t)target_image - (uintptr_t)image_base;
 
     // Relocate local_image, to ensure that it will have correct addresses once it's in the target process
-    IMAGE_BASE_RELOCATION* relocation_table = (IMAGE_BASE_RELOCATION*)(local_image + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+    IMAGE_BASE_RELOCATION* reloc_table = (IMAGE_BASE_RELOCATION*)(local_image + nt_header->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
 
-    while (relocation_table->SizeOfBlock > 0) {
+    while (reloc_table->SizeOfBlock > 0) {
 	  	// BASE_RELOCATION_ENTRY is 2 bytes, so the size (minus header) over 2 is our entry count
 		// (not using sizeof(BASE_RELOCATION_ENTRY) because some compilers might add padding or something on bitfields)
-        uint32_t relocation_entries_count = (relocation_table->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(uint16_t);
+        uint32_t entries_count = (reloc_table->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(uint16_t);
 		// This VirtualAddress member tells us the offset of the relocation table from the image base.
-        BASE_RELOCATION_ENTRY* relative_addresses = (BASE_RELOCATION_ENTRY*)((uintptr_t)relocation_table->VirtualAddress + local_image);
+        BASE_RELOCATION_ENTRY* relative_addresses = (BASE_RELOCATION_ENTRY*)((uintptr_t)reloc_table->VirtualAddress + local_image);
 
-        for (uint32_t i = 0; i < relocation_entries_count; i++) {
+        for (uint32_t i = 0; i < entries_count; i++) {
             if (relative_addresses[i].Offset) {
 			  	// Get the address of the pointer we need to relocate, then add our image base delta.
                 uintptr_t* patched_address = (uintptr_t*)(relative_addresses + relative_addresses[i].Offset);
-                *patched_address += delta_image_base;
+                *patched_address += reloc_bias;
             }
         }
 		// Go to the next relocation table and repeat. There is one table per 4KiB page.
-        relocation_table = (IMAGE_BASE_RELOCATION*)((uintptr_t)relocation_table + relocation_table->SizeOfBlock);
+        reloc_table = (IMAGE_BASE_RELOCATION*)((uintptr_t)reloc_table + reloc_table->SizeOfBlock);
     }
 
     // Write the relocated PE into the target process
@@ -88,7 +88,8 @@ bool self_inject(uint32_t process_id, LPTHREAD_START_ROUTINE entry_point, void* 
     free(local_image);
 
     // Start the injected PE inside the target process
-    CreateRemoteThread(target_process, NULL, 0, (LPTHREAD_START_ROUTINE)((uint8_t*)entry_point + delta_image_base), parameter, 0, NULL);
+    CreateRemoteThread(target_process, NULL, 0, (LPTHREAD_START_ROUTINE)((uint8_t*)entry_point + reloc_bias), parameter, 0, NULL);
+	CloseHandle(target_process);
 
     return true;
 }
